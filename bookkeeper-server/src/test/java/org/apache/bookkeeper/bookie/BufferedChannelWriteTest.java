ef99b8a6b4c20d2b06530a7f39a87b08d54c3283
francesco <donnini.francesco00@gmail.com>
2026-03-19 16:58:10 +0100

    aggiunti test per il metodo write()

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelWriteTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelWriteTest.java
@@ -3,155 +3,144 @@ package org.apache.bookkeeper.bookie;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.NonWritableChannelException;
 import java.nio.file.*;
-import java.time.Duration;
 import java.util.*;
 import java.util.stream.Stream;
 
 public class BufferedChannelWriteTest {
     private static final byte[] EMPTY = new byte[0];
 
-    // c_W, U_B, |src|, |ω| perm, opts
-    private static Stream<Arguments> newInputs() {
+    @Test
+    public void nullSourceTest() throws IOException {
+        Path path = TemporaryFile.create("file", "rw-rw-r--");
+        try (FileChannel fc = FileChannel.open(path, setOf(StandardOpenOption.WRITE));
+             BufferedChannel b = new BufferedChannel(ByteBufAllocator.DEFAULT, fc, 100, 1)) {
+            Assertions.assertThrows(NullPointerException.class, () -> b.write(null));
+        } finally {
+            Files.delete(path);
+        }
+    }
+
+    @Test
+    public void emptySourceTest() throws IOException {
+        Path path = TemporaryFile.create("file", "rw-rw-r--");
+        try (FileChannel fc = FileChannel.open(path, setOf(StandardOpenOption.WRITE));
+             BufferedChannel b = new BufferedChannel(ByteBufAllocator.DEFAULT, fc, 1)) {
+            ByteBuf empty = ByteBufAllocator.DEFAULT.buffer(0);
+            b.write(empty);
+            Assertions.assertEquals(0, b.getUnpersistedBytes());
+            Assertions.assertEquals(0, b.getNumOfBytesInWriteBuffer());
+            Assertions.assertEquals(0, b.getFileChannelPosition());
+            Assertions.assertEquals(0, b.position());
+            Assertions.assertEquals(0, b.size());
+        } finally {
+            Files.delete(path);
+        }
+    }
+
+    private static Stream<Arguments> readOnlyFileTestInputs() {
         return Stream.of(
-                // Test#1: c_W = U_B = 0
-                // Arguments.of(0, 0, 1, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(1, 1, 2, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(2047, 2046, 1024, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(2048, 2049, 667, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(511, 510, 512, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(411, 412, 413, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                // Arguments.of(0, 0, 1125, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(1233, 0, 1, 1232, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(1233, 0, 2, 1232, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(1233, 665, 1, 664, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(1233, 665, 2, 664, "rw-rw-r--", setOf(StandardOpenOption.WRITE), "Hello, World!".getBytes()),
-                Arguments.of(10, 0, 0, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(20, 10, 10, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(20, 10, 15, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY),
-                Arguments.of(20, 10, 5, 0, "rw-rw-r--", setOf(StandardOpenOption.WRITE), EMPTY)
+                Arguments.of(3, 2, 2),
+                Arguments.of(3, 2, 4)
         );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("readOnlyFileTestInputs")
+    public void readOnlyFileTest(int inputSize, int unpersistedBytesBound, int writeCapacity) throws IOException {
+        Path path = TemporaryFile.create("ro", "r--r--r--");
+        try (FileChannel fc = FileChannel.open(path, setOf(StandardOpenOption.READ));
+             BufferedChannel b = new BufferedChannel(ByteBufAllocator.DEFAULT, fc, writeCapacity, unpersistedBytesBound)) {
+            ByteBuf src = ByteBufAllocator.DEFAULT.buffer(8);
+            src.writeBytes(randomBytes(inputSize));
+            Assertions.assertThrows(NonWritableChannelException.class, () -> b.write(src));
+        } finally {
+            Files.delete(path);
+        }
+    }
+
+    private static Stream<Arguments> singleWriteInput() {
+        return Stream.of(
+                Arguments.of(2, 3, 1),
+                Arguments.of(2, 1, 3),
+                Arguments.of(1, 2, 3),
+                Arguments.of(1, 3, 2),
+                Arguments.of(4, 3, 2),
+                Arguments.of(3, 1, 2),
+                Arguments.of(3, 0, 2),
+                Arguments.of(3, 0, 3),
+                Arguments.of(20, 10, 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleWriteInput")
+    public void singleWriteTest(int inputSize, int unpersistedBytesBound, int writeCapacity) throws IOException {
+        Path path = TemporaryFile.create("file", "rw-rw-r--");
+        try (FileChannel fc = FileChannel.open(path, setOf(StandardOpenOption.WRITE, StandardOpenOption.READ));
+             BufferedChannel bc = new BufferedChannel(ByteBufAllocator.DEFAULT, fc, writeCapacity, unpersistedBytesBound)) {
+            byte[] data = randomBytes(inputSize);
+            ByteBuf src = ByteBufAllocator.DEFAULT.buffer(data.length);
+
+            int writeBufferSize = bc.getNumOfBytesInWriteBuffer();
+            long unpersistedBytes = bc.getUnpersistedBytes();
+            long filePosition = bc.getFileChannelPosition();
+            long position = bc.position();
+
+            src.writeBytes(data);
+            bc.write(src);
+
+            if (writeBufferSize + data.length >= writeCapacity || (unpersistedBytesBound > 0 && unpersistedBytes + data.length >= unpersistedBytesBound)) {
+                ByteBuffer actual = ByteBuffer.allocate(data.length);
+                int br = fc.read(actual, filePosition);
+                Assertions.assertArrayEquals(Arrays.copyOfRange(data, 0, br), Arrays.copyOfRange(actual.array(), 0, br));
+            } else {
+                ByteBuf actual = ByteBufAllocator.DEFAULT.buffer(data.length);
+                bc.read(actual, position);
+                Assertions.assertEquals(data.length, actual.readableBytes());
+                for (int i = 0; i < data.length; i++) {
+                    Assertions.assertEquals(data[i], actual.getByte(i));
+                }
+            }
+            Assertions.assertEquals(position + data.length, bc.position());
+        } finally {
+            Files.delete(path);
+        }
+    }
+
+    private static Stream<Arguments> multipleWritesInput() {
+        return Stream.of(
+                Arguments.of(2, 3, 1),
+                Arguments.of(2, 1, 3),
+                Arguments.of(1, 2, 3),
+                Arguments.of(1, 3, 2),
+                Arguments.of(4, 3, 2),
+                Arguments.of(3, 1, 2),
+                Arguments.of(3, 0, 2),
+                Arguments.of(3, 0, 3),
+                Arguments.of(20, 10, 5)
+        );
+    }
+
+    private static byte[] randomBytes(int length) {
+        byte[] bytes = new byte[length];
+        new Random().nextBytes(bytes);
+        return bytes;
     }
 
     private static Set<OpenOption> setOf(OpenOption ...options) {
         Set<OpenOption> set = new HashSet<>();
         Collections.addAll(set, options);
         return set;
-    }
-
-    @ParameterizedTest
-    @MethodSource("newInputs")
-    public void parametricSingleWriteTest(
-            int writeCapacity,
-            int unpersistedByteBound,
-            int writableBytes,
-            int initialBufferSize,
-            String permissions,
-            Set<OpenOption> options,
-            byte[] initialFileContent) throws IOException {
-        String summary = "writeCapacity=" + writeCapacity
-                + ", unpersistedByteBound=" + unpersistedByteBound
-                + ", n=" + writableBytes
-                + ", ω=" + initialBufferSize
-                + ", |f|=" + initialFileContent.length;
-
-        Path path;
-        if (initialFileContent.length == 0) {
-            path = TemporaryFile.create("file", permissions);
-        } else {
-            path = TemporaryFile.create("file", permissions, initialFileContent);
-        }
-
-        ByteBuf src = ByteBufAllocator.DEFAULT.buffer(writeCapacity);
-        int flushedBytes;
-        long oldFileChannelPosition;
-        try (FileChannel channel = FileChannel.open(path, options);
-             BufferedChannel b = new BufferedChannel(ByteBufAllocator.DEFAULT, channel, writeCapacity, unpersistedByteBound)) {
-            // Riempi il buffer di scrittura di esattamente initialBufferSize byte(s)
-            if (initialBufferSize > 0) {
-                ByteBuf initialContent = ByteBufAllocator.DEFAULT.buffer(initialBufferSize);
-                b.write(initialContent);
-                initialContent.release();
-            }
-            BufferedChannelState old = BufferedChannelState.of(b);
-
-            // Crea il buffer sorgente di dimensione writableBytes
-            if (writableBytes > 0) {
-                src.writeBytes(bytes((byte)50, writableBytes));
-            }
-
-            // Timeout di 10 s perché ci sono dei casi di test che rimangono incastrati in un loop infinito
-            Assertions.assertTimeoutPreemptively(Duration.ofSeconds(5), () -> b.write(src), testFailed(summary));
-
-            // In seguito alla scrittura isolata di writableBytes byte(s) la posizione assoluta della prossima scrittura deve essere
-            // avanzata di tanti byte quanti sono quelli che sono stati scritti!
-            Assertions.assertEquals(old.getPosition() + writableBytes, b.position(), testFailed(summary));
-
-            if (shouldFlush(writableBytes, writeCapacity, unpersistedByteBound)) {
-                // Se è necessario un flush bisogna distinguere i due casi in cui il flush avviene a causa dello scavalcamento
-                // della soglia oppure per il riempimento del buffer di scrittura
-                if (unpersistedByteBound > 0) {
-                    flushedBytes = writableBytes;
-                } else {
-                    flushedBytes = writeCapacity;
-                }
-                Assertions.assertEquals(
-                        old.getFileChannelPosition() + flushedBytes,
-                        b.getFileChannelPosition(),
-                        testFailed(summary));
-                // Si controlla se il contenuto del buffer è stato riversato nel file sottostante
-                oldFileChannelPosition = old.getFileChannelPosition();
-                Assertions.assertEquals(0, b.getUnpersistedBytes(), testFailed(summary));
-                checkUnderlyingFile(path, (int) oldFileChannelPosition, flushedBytes, src);
-            } else {
-                Assertions.assertEquals(old.getFileChannelPosition(), b.getFileChannelPosition(), testFailed(summary));
-                Assertions.assertEquals(old.getNumOfBytesInWriteBuffer() + writableBytes, b.getNumOfBytesInWriteBuffer(), testFailed(summary));
-                // se unpersistedByteBound > 0 ma non è stato effettuato alcun flush allora bisogna verificare che il numero di bytes accumulati è stato
-                // aggiornato correttamente
-                if (unpersistedByteBound > 0) {
-                    Assertions.assertEquals(old.getUnpersistedBytes() + writableBytes, b.getUnpersistedBytes(), testFailed(summary));
-                }
-            }
-        } finally {
-            src.release();
-        }
-        Files.delete(path);
-    }
-
-    private static byte[] bytes(byte b, int n) {
-        byte[] bytes = new byte[n];
-        Arrays.fill(bytes, b);
-        return bytes;
-    }
-
-    private static String testFailed(String summary) {
-        return String.format("Test %s failed", summary);
-    }
-
-    private boolean shouldFlush(int writeBytes, int writeCapacity, int unpersistedByteBound) {
-        if (unpersistedByteBound > 0) {
-            return writeBytes >= Math.min(writeCapacity, unpersistedByteBound);
-        } else {
-            return writeBytes > writeCapacity;
-        }
-    }
-
-    private void checkUnderlyingFile(Path path, int position, int n, ByteBuf expected) {
-        try (FileInputStream reader = new FileInputStream(path.toFile())) {
-            byte[] actual = new byte[n];
-            int bytesRead = reader.read(actual, position, n);
-            Assertions.assertEquals(n, bytesRead);
-            byte[] expectedByteArray = new byte[n];
-            expected.getBytes(0, expectedByteArray);
-            Assertions.assertArrayEquals(expectedByteArray, actual);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
